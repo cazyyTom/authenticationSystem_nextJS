f4cac043d2016b568806f5958a380ea009889b1e
devesh tomar <tomardevesh012@gmail.com>
2026-07-03 01:18:34 +0530

    Improved signup page

--- my-app/src/app/signup/page.tsx
+++ my-app/src/app/signup/page.tsx
@@ -1,6 +1,10 @@
-
+"use client";
 
 export default function SignupPage() {
+
+
+
+
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-gray-950">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
